cef8ac1fcc9b29b7519d5b90c3f8b306df1e7e43
Ben Paschke <ben@ioxu.com>
2021-11-16 01:32:38 +1030

    better options dui

--- scenes/player/options_menu_2d.gd
+++ scenes/player/options_menu_2d.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 var global_time = 0
 
-onready var db = $debug2
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	global_time += delta
-	db.rect_position = Vector2( 0.0, fmod(global_time * 150 ,512.0) )
+#	global_time += delta
+#	db.rect_position = Vector2( 0.0, fmod(global_time * 150 ,512.0) )
+	pass
